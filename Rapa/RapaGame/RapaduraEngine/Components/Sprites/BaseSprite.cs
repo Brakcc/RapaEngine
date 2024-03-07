af92566321ee9f2822a403d4cd55a2a1e5d12544
Brakcc <clement.strappazzon.marques@outlook.fr>
2024-03-07 01:46:25 +0100

    more components
    penser aux Shaders !!!!!!!!!

--- Rapa/RapaGame/RapaduraEngine/Components/Sprites/BaseSprite.cs
+++ Rapa/RapaGame/RapaduraEngine/Components/Sprites/BaseSprite.cs
@@ -1,10 +1,26 @@
-﻿using Rapa.RapaGame.RapaduraEngine.Entities;
+﻿using Microsoft.Xna.Framework.Graphics;
+using Rapa.RapaGame.RapaduraEngine.Entities;
 
 namespace Rapa.RapaGame.RapaduraEngine.Components.Sprites;
 
 public class BaseSprite : Component
 {
+    #region constructor
+    
     public BaseSprite(Entity entityRef) : base(entityRef)
     {
+        _texture = null;
     }
+
+    public BaseSprite(Entity entityRef, Texture2D texture) : base(entityRef)
+    {
+        _texture = texture;
+    }
+    #endregion
+
+    #region fields
+
+    private Texture2D _texture;
+
+    #endregion
 }
